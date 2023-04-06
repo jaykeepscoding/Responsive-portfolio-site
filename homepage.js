adf9f6d439001c96f59622a897273cfdf9ba2641
jaynuet <jaynuet@outlook.com>
2023-04-06 15:47:27 -0500

    Added loader-animation

--- homepage.js
+++ homepage.js
@@ -1,12 +1,14 @@
 var x = document.getElementById("menuList");
 
-function toggleMenu(){
-  if (x.style.maxHeight == '0px')
-  {
-      x.style.maxHeight = '130px';
+$(window).on("load", function () {
+  $(".loader-wrapper").fadeOut("slow");
+});
+
+function toggleMenu() {
+  if (x.style.maxHeight == '0px') {
+    x.style.maxHeight = '130px';
   }
-  else
-  {
-      x.style.maxHeight = '0px';
+  else {
+    x.style.maxHeight = '0px';
   }
 }
